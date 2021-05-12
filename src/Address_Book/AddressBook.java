3bfd08faefbd877528c756b818ac99a0966c2dc1
ayurn <65171090+ayurn@users.noreply.github.com>
2021-05-12 11:12:45 +0530

    Added : adding contact

---
+++ src/Address_Book/AddressBook.java
@@ -0,0 +1,12 @@
+package Address_Book;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+    public ArrayList<ContactDetails> contactList = new ArrayList<>();
+
+    public  ArrayList<ContactDetails> contactList(ContactDetails cd){
+        contactList.add(cd);
+        return contactList;
+    }
+}
